fa68529cc7ce8d122d44533447a67977d2e75f34
Chris Borchert <cborchertrun@gmail.com>
2018-07-26 18:16:54 +0200

    Add some default text and a clear button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ZipfGraph from './components/ZipfGraph';
 import './App.css';
+import MobyDick from './MobyDick.js'
 
 class App extends Component {
   state = {
-    text: "type or paste some text here to check its zipfiness..."
+    text: "type or paste some text here to check its zipfiness...\n"+MobyDick
   }
 
   handleTextChange(e){
@@ -13,12 +14,32 @@ class App extends Component {
     });
   }
 
+  handleClear(e){
+    this.setState({
+      text: ""
+    });
+  }
+
   render() {
     const {text} = this.state;
     return (
       <div className="App">
-        <textarea value={text} onChange={this.handleTextChange.bind(this)} />
+        <div className="App__header">
+          <h1 className="App__title">Zipf-y</h1>
+        </div>
+        <div className="App__textarea-container">
+          <textarea value={text} onChange={this.handleTextChange.bind(this)} />
+          <button onClick={this.handleClear.bind(this)}>Clear</button>
+        </div>
         <ZipfGraph text={text} />
+        <div className="App__note">
+          <strong>Note:</strong> yellow dots show expected frequency of the n<sup>th</sup> most frequent word, assuming <em>frequency = 1/n</em>
+        </div>
+        <div className="App__credits">
+          inspired by vsauce's <a href="https://www.youtube.com/watch?v=fCn8zs912OE" target="_blank">the zipf mystery video</a><br/>
+          see the <a href="https://github.com/cborchert/zipfy" target="_blank">github repo</a><br/>
+          created by: <a href="https://cborchert.com" target="_blank">chris borchert</a><br/>
+        </div>
       </div>
     );
   }
